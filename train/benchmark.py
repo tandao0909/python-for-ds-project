6b7af9ebc6fec9cfa985cb79649bc1d2515b59be
tandao0909 <tan.daoxuan0909@gmail.com>
2024-12-29 15:38:00 +0700

    Add evaluation step

---
+++ train/benchmark.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+from typing import Any
+
+import pandas as pd
+
+from sklearn.svm import SVR
+from sklearn.linear_model import Ridge
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+
+from constants import TEST_PATH, TARGET_COLUMN, BENCHMARK_DIRPATH
+from utils import train_default_models
+
+MODEL_DICT: dict[str, Any]  = {
+    "Ridge": Ridge(),
+    "Tree": DecisionTreeRegressor(
+        criterion='poisson',
+        max_depth=50,
+        max_features=None,
+        max_leaf_nodes=50,
+        min_impurity_decrease=0.0,
+        min_samples_leaf=4,
+        min_samples_split=2,
+        min_weight_fraction_leaf=0.0,
+        splitter='best'
+    ),
+    "Rbf": SVR(kernel="rbf"),
+    "Extra Tree": ExtraTreesRegressor(n_jobs=-1),
+}
+BENCHMARK_PATH = os.path.join(BENCHMARK_DIRPATH, "evaluate.csv")
+
+def train_ensemble_models(
+    X: pd.DataFrame, y: pd.Series
+):
+    Path(BENCHMARK_PATH).parent.mkdir(parents=True, exist_ok=True)
+
+    # Clear all content in the benchmark file
+    with open(BENCHMARK_PATH, "w") as file:
+        pass
+    print("Evaluate models using the fine tuned parameters.")
+    train_default_models(X, y, model_dict=MODEL_DICT, benchmark_path=BENCHMARK_PATH)
+    print(f"See the evaluation results at {BENCHMARK_PATH}")
+
+
+if __name__ == "__main__":
+    data = pd.read_csv(TEST_PATH)
+    X = data.drop(TARGET_COLUMN, axis=1)
+    y = data[TARGET_COLUMN]
+    train_ensemble_models(X, y)
